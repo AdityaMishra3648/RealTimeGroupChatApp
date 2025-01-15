8634d2f7658126a9905d5f642c9d3dcef0e94e58
adityamishra3648 <mk6748134@gmail.com>
2025-01-15 16:55:55 +0530

    just added a hii path

---
+++ src/main/java/com/chatgroup/demo/test.java
@@ -0,0 +1,14 @@
+package com.chatgroup.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/chatapp")
+public class test {
+    @GetMapping("/greet")
+    public String greet(){
+        return "hii there bro";
+    }
+}
